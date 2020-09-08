452675a0b032b3f233447bb39edda14fadb5384d
Влада Дегтярёва <vadegtyaryova@edu.hse.ru>
2020-09-08 23:57:27 +0500

    Task01

--- Module01/Homework01/Task01/Task01/Program.cs
+++ Module01/Homework01/Task01/Task01/Program.cs
@@ -6,7 +6,7 @@ namespace Task01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, World!");
             Console.ReadKey();
         }
     }
