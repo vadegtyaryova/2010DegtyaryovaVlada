272dd8709960549ac28419ab3dce7250b14983c6
Влада Дегтярёва <vadegtyaryova@edu.hse.ru>
2020-09-09 00:16:41 +0500

    Task10

---
+++ Module01/Seminar01/Task10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            string[] words = new string[n];
+            Console.WriteLine($"Введите {n} слов(а): ");
+            for(int i = 0; i < n; i++)
+            {
+                words[i] = Console.ReadLine();
+            }
+            Console.Write(words[0]);
+            for( int i = 1; i < n; i++)
+            {
+                Console.Write($"!{words[i]}");
+            }
+        }
+    }
+}
